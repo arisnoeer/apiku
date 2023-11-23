134c55858b22fa86ae3230b30cd5ef4c0e7f02fd
arisnoeer <76475296+arisnoeer@users.noreply.github.com>
2023-11-24 04:27:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'ohmyriiss28'// yourname
+global.MONGO_DB_URI = "anDq62IUmAawWNQL" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "arispurwanto2822@gmail.com" //email
+global.email_password = "lbpf hrig wwri odeo" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
